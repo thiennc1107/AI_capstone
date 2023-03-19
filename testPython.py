2bd58eeba5b200f911aca2afee1e327cc6a01b78
ThieNguyen <thiennc1107@gmail.com>
2023-03-19 15:17:08 +0700

    fix

--- testPython.py
+++ testPython.py
@@ -1,16 +1,13 @@
-import serial
-import random
-import time
 import speedController as sp
 
-speedCtrl = sp.SpeedController()
-speedCtrl.set_speed(0, 0)
-speedCtrl.set_speed(10,10)
-time.sleep(3)
-speedCtrl.set_speed(20, 10)
-time.sleep(1)
-speedCtrl.set_speed(10,10)
-time.sleep(3)
-speedCtrl.set_speed(0, 0)
+controller = sp.SpeedController()
+controller.set_speed(0, 0)
+
+while True:
+    data = input("input speed: ")
+    speedArr = data.split(' ')
+    speed1 = int(speedArr[0])
+    speed2 = int(speedArr[1])
+    controller.set_speed(speed1, speed2)
 
 
